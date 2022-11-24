6233fb563718e436100828379a7b66c053145afa
Ihor Shapoval <ihor.shapoval@vaimo.com>
2022-11-24 11:30:04 +0100

    removed unused param

--- src/api/paramTransformer.js
+++ src/api/paramTransformer.js
@@ -1,7 +1,6 @@
 import { query } from './query';
 
 export const transformParam = (search, offset) => {
-    const usedAttributes = ['name', 'sku'];
     const pageSize = 20;
     const currentPage = offset ? (pageSize + offset) / pageSize : 1; 
     return JSON.stringify({query, variables: {search, pageSize, currentPage}})
