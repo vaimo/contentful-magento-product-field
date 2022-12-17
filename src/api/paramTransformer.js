4c8f563605fbf8806f4514b28fe8929fc0074838
Ihor Shapoval <ihor.shapoval@vaimo.com>
2022-12-17 19:13:06 +0100

    removed old js variations of files

--- src/api/paramTransformer.js
+++
@@ -1,6 +0,0 @@
-import { query } from './query';
-export const transformParam = (search, offset) => {
-    const pageSize = 20;
-    const currentPage = offset ? (pageSize + offset) / pageSize : 1;
-    return JSON.stringify({ query, variables: { search, pageSize, currentPage } });
-};
