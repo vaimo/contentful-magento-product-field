58f6a771c63ca3b635b67bf3c8d0e594f9d1505d
Ihor Shapoval <ihor.shapoval@vaimo.com>
2023-07-24 00:04:39 +0200

    DPTQ-3187 - changed way of making calls to API from graphQL to REST

--- src/api/paramTransformer.ts
+++ src/api/paramTransformer.ts
@@ -1,7 +1,31 @@
-import { query } from './query';
-
-export const transformParam = (search: string, offset: number): string => {
-  const pageSize = 20;
-  const currentPage = offset ? (pageSize + offset) / pageSize : 1; 
-  return JSON.stringify({query, variables: {search, pageSize, currentPage}})
+export const transformParam = (param, offset) => {
+  const usedAttributes = ['name', 'sku'];
+  const PAGE_SIZE = 20;
+  const currentPage = offset ? (PAGE_SIZE + offset) / PAGE_SIZE : 1; 
+  const res = [
+      ['fields', 'items[id,sku,name,media_gallery_entries]total_count'],
+      ['searchCriteria[sortOrders][0][field]', 'id'],
+      ['searchCriteria[filter_groups][1][filters][0][field]', 'visibility'],
+      ['searchCriteria[filter_groups][1][filters][0][condition_type]', 'eq'],
+      ['searchCriteria[filter_groups][1][filters][0][value]', '4'],
+      ['searchCriteria[pageSize]', PAGE_SIZE],
+      ['searchCriteria[currentPage]', currentPage]
+  ];
+  if (param) {
+      usedAttributes.forEach((attr, index) => {
+          res.push([
+              `searchCriteria[filter_groups][0][filters][${index}][field]`, attr
+          ]);
+          res.push([
+              `searchCriteria[filter_groups][0][filters][${index}][value]`, `%25${param}%25`
+          ]);
+          res.push([
+              `searchCriteria[filter_groups][0][filters][${index}][condition_type]`, "like"
+          ])
+      });
+  } else {
+      res
+  } 
+  //@ts-ignore
+  return new URLSearchParams(res).toString();
 }
